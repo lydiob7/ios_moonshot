57cd4a58ea48ea1d0719593d58cd888c93b802b9
lydiob7 <lydiob7@tutanota.com>
2024-08-07 16:32:27 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tomi Scattini on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
